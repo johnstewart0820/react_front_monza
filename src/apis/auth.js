b40064191db27f25cb286ce1ee0db666218b41f6
johnstewart0820 <john.s@outsource.codes>
2021-06-03 15:50:10 +0300

    auth apis minor fix

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -1,79 +1,59 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 import storage from '../utils/storage';
-class Auth {
-    login = (email, password) => {
-        return axios
-            .post(`${process.env.REACT_APP_BACKEND_URL}/auth/login`, {
-                email: email,
-                password: password
-            })
-            .then(response => {
-                if (response.data.code === 200) {
-                    storage.setStorage('token', response.data.data.token);
+
+import API from "./API";
+
+const Auth = {
+    login: ( email, password ) => {
+        return API
+            .post(`/auth/login`, { email, password })
+            .then( res => {
+                if ( res.data.code === 200) {
+                    storage.setStorage('token', res.data.data.token);
                 }
-                return response.data;
-            }).catch(error => {
-                return error;
-            })
-    }
 
-    register = (email, password) => {
-        return axios
-            .post(`${process.env.REACT_APP_BACKEND_URL}/auth/register`, {
-                email: email,
-                password: password,
-            })
-            .then(response => {
-                return response.data;
-            }).catch(error => {
-                return error;
-            })
-    }
+                return res.data;
 
-    validate = (token) => {
+            }).catch( error => error )
+    },
+
+    register: ( email, password ) => {
+        return API
+            .post(`/auth/register`, { email, password })
+            .then( res => res.data )
+			.catch( error => error )
+    },
+
+    validate: token => {
         return axios
-            .post(`${process.env.REACT_APP_BACKEND_URL}/auth/verify`, {
-            }, {headers: authHeader(token)})
-            .then(response => {
-                return response.data;
-            }).catch(error => {
-                return error;
-            })
-    }
+            .post(`${process.env.REACT_APP_BACKEND_URL}/auth/verify`, 
+				{}, { headers: authHeader( token )}
+			)
+            .then( res => res.data )
+			.catch( err => err )
+    },
     
-    forgot = (email) => {
-        return axios
-            .post(`${process.env.REACT_APP_BACKEND_URL}/auth/forgot`, {
-                email: email
-            })
-            .then(response => {
-                return response.data;
-            }).catch(error => {
-                return error;
-            })
-    }
+    forgot: email => {
+        return API
+            .post(`/auth/forgot`, { email })
+            .then( res => res.data )
+			.catch( err => err )
+    },
 
-    reset_password = (password, token) => {
+    reset_password: ( password, token ) => {
         return axios
-            .post(`${process.env.REACT_APP_BACKEND_URL}/auth/reset_password`, {
-                password: password
-            }, {headers: authHeader(token)})
-            .then(response => {
-                return response.data;
-            }).catch(error => {
-                return error;
-            })
+            .post(`${process.env.REACT_APP_BACKEND_URL}/auth/reset_password`, { password }, { headers: authHeader( token )})
+            .then( res => res.data )
+			.catch( err => err )
+    },
+	
+    validateToken: () => {
+        return API
+        	.get(`/user/validate_token`)
+        	.then( res => res.data )
+			.catch( err => err )  
     }
-    validateToken = () => {
-        return axios
-        .get(`${process.env.REACT_APP_BACKEND_URL}/user/validate_token`, {headers: authHeader(storage.getStorage('token'))})
-        .then(response => {
-            return response.data;
-        }).catch(error => {
-            return error;
-        })    
-    }
-
 }
-export default new Auth();
+
+export default Auth;
