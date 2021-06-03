03a927674540b552e330c1ae80b752586eadad86
johnstewart0820 <john.s@outsource.codes>
2021-06-03 16:27:41 +0300

    apis/Contractor changes

--- src/apis/contractor.js
+++ src/apis/contractor.js
@@ -1,144 +1,66 @@
-import axios from 'axios';
-import authHeader from './auth-header';
-import storage from '../utils/storage';
-class Contractor {
-    getInfo = (id) => {
-        return axios
-            .get(`${process.env.REACT_APP_BACKEND_URL}/contractor/info`, {
-                headers: authHeader(storage.getStorage('token')),
-            })
-            .then(response => {
-                if (response.data.code === 401) {
-                    storage.removeStorage('token');
-                    storage.removeStorage('role');
-                    return response.data;
-                } else if (response.data.code === 200) {
-                    return response.data;
-                }
-            }).catch(error => {
-                return error;
-            })
-    }
-    get = (id) => {
-        return axios
-            .get(`${process.env.REACT_APP_BACKEND_URL}/contractor`, {
-                headers: authHeader(storage.getStorage('token')),
-                params: {
-                    id: id
-                },
-            })
-            .then(response => {
-                if (response.data.code === 401) {
-                    storage.removeStorage('token');
-                    storage.removeStorage('role');
-                    return response.data;
-                } else if (response.data.code === 200) {
-                    return response.data;
-                }
-            }).catch(error => {
-                return error;
-            })
-    }
+import API from "./API";
 
-    export = (id) => {
-        return axios
-            .get(`${process.env.REACT_APP_BACKEND_URL}/contractor/export`, {
-                headers: authHeader(storage.getStorage('token')),
-            })
-            .then(response => {
-                if (response.data.code === 401) {
-                    storage.removeStorage('token');
-                    storage.removeStorage('role');
-                    return response.data;
-                } else if (response.data.code === 200) {
-                    return response.data;
-                }
-            }).catch(error => {
-                return error;
-            })
-    }
+const Contractor = {
+    getInfo: () => {
+        return API
+            .get(`/contractor/info`)
+            .then( res => res.data )
+			.catch( error => error )
+    },
 
-    create = (data) => {
-        return axios
-            .post(`${process.env.REACT_APP_BACKEND_URL}/contractor`, {
-                data: data
-            }, {
-                headers: authHeader(storage.getStorage('token'))
-            }).then(response => {
-                if (response.data.code === 401)
-                    storage.removeStorage('token');
-                return response.data;
-            }).catch(error => {
-                return error;
-            })
-    }
+    get: id => {
+        return API
+            .get(`/contractor`, { params: { id }})
+            .then( res => res.data )
+			.catch( error => error )
+    },
 
-    create_list = (data) => {
-        return axios
-            .post(`${process.env.REACT_APP_BACKEND_URL}/contractor/list`, {
-                data: data
-            }, {
-                headers: authHeader(storage.getStorage('token'))
-            }).then(response => {
-                if (response.data.code === 401)
-                    storage.removeStorage('token');
-                return response.data;
-            }).catch(error => {
-                return error;
-            })
-    }
+    export: id => {
+        return API
+            .get(`/contractor/export`)
+            .then( res => res.data )
+			.catch( error => error )
+    },
 
-    getListByOption = (sort_option, count, page, search_option) => {
-        return axios
-            .post(`${process.env.REACT_APP_BACKEND_URL}/contractor/filter_list`, {
+    create: data => {
+        return API
+            .post(`/contractor`, { data })
+			.then( res => res.data )
+			.catch( error => error )
+    },
+
+    create_list: data => {
+        return API
+            .post(`/contractor/list`, { data })
+			.then( res => res.data )
+			.catch( error => error )
+    },
+
+    getListByOption: (sort_option, count, page, search_option) => {
+        return API
+            .post(`/contractor/filter_list`, {
                 sort_option: sort_option,
                 count: count,
                 page: page,
                 search_option: search_option
-            }, {
-                headers: authHeader(storage.getStorage('token'))
             })
-            .then(response => {
-                if (response.data.code === 401)
-                    storage.removeStorage('token');
-                return response.data;
-            }).catch(error => {
-                return error;
-            })
-    }
+            .then( res => res.data )
+			.catch( error => error )
+    },
 
-    update = (data, id) => {
-        return axios
-            .put(`${process.env.REACT_APP_BACKEND_URL}/contractor`, {
-                data: data,
-                id: id
-            }, {
-                headers: authHeader(storage.getStorage('token'))
-            })
-            .then(response => {
-                if (response.data.code === 401)
-                    storage.removeStorage('token');
-                return response.data;
-            }).catch(error => {
-                return error;
-            })
-    }
+    update: (data, id) => {
+        return API
+            .put(`/contractor`, { data, id })
+            .then( res => res.data )
+			.catch( error => error )
+    },
 
-    delete = (id) => {
-        return axios
-            .delete(`${process.env.REACT_APP_BACKEND_URL}/contractor`, {
-                headers: authHeader(storage.getStorage('token')),
-                params: {
-                    id: id
-                },
-            })
-            .then(response => {
-                if (response.data.code === 401)
-                    storage.removeStorage('token');
-                return response.data;
-            }).catch(error => {
-                return error;
-            })
+    delete: id => {
+        return API
+            .delete(`/contractor`, { params: { id }})
+            .then( res => res.data )
+			.catch( error => error )
     }
 }
-export default new Contractor();
+
+export default Contractor;
