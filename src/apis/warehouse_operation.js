b5b7f13b26513915e24de16b44e4156cbd759d5f
johnstewart0820 <john.s@outsource.codes>
2021-06-03 16:44:16 +0300

    apis/WarehouseOperation changes

--- src/apis/warehouse_operation.js
+++ src/apis/warehouse_operation.js
@@ -1,144 +1,66 @@
-import axios from 'axios';
-import authHeader from './auth-header';
-import storage from '../utils/storage';
-class WarehouseOperation {
-    getInfo = (id) => {
-        return axios
-            .get(`${process.env.REACT_APP_BACKEND_URL}/warehouse_operation/info`, {
-                headers: authHeader(storage.getStorage('token')),
-            })
-            .then(response => {
-                if (response.data.code === 401) {
-                    storage.removeStorage('token');
-                    storage.removeStorage('role');
-                    return response.data;
-                } else if (response.data.code === 200) {
-                    return response.data;
-                }
-            }).catch(error => {
-                return error;
-            })
-    }
-    get = (id) => {
-        return axios
-            .get(`${process.env.REACT_APP_BACKEND_URL}/warehouse_operation`, {
-                headers: authHeader(storage.getStorage('token')),
-                params: {
-                    id: id
-                },
-            })
-            .then(response => {
-                if (response.data.code === 401) {
-                    storage.removeStorage('token');
-                    storage.removeStorage('role');
-                    return response.data;
-                } else if (response.data.code === 200) {
-                    return response.data;
-                }
-            }).catch(error => {
-                return error;
-            })
-    }
+import API from "./API";
 
-    export = (id) => {
-        return axios
-            .get(`${process.env.REACT_APP_BACKEND_URL}/warehouse_operation/export`, {
-                headers: authHeader(storage.getStorage('token')),
-            })
-            .then(response => {
-                if (response.data.code === 401) {
-                    storage.removeStorage('token');
-                    storage.removeStorage('role');
-                    return response.data;
-                } else if (response.data.code === 200) {
-                    return response.data;
-                }
-            }).catch(error => {
-                return error;
-            })
-    }
+const WarehouseOperation = {
+    getInfo: () => {
+        return API
+            .get(`/warehouse_operation/info`)
+            .then( res => res.data )
+			.catch( error => error )
+    },
 
-    create = (data) => {
-        return axios
-            .post(`${process.env.REACT_APP_BACKEND_URL}/warehouse_operation`, {
-                data: data
-            }, {
-                headers: authHeader(storage.getStorage('token'))
-            }).then(response => {
-                if (response.data.code === 401)
-                    storage.removeStorage('token');
-                return response.data;
-            }).catch(error => {
-                return error;
-            })
-    }
+    get: id => {
+        return API
+            .get(`/warehouse_operation`, { params: { id }})
+            .then( res => res.data )
+			.catch( error => error )
+    },
 
-    create_list = (data) => {
-        return axios
-            .post(`${process.env.REACT_APP_BACKEND_URL}/warehouse_operation/list`, {
-                data: data
-            }, {
-                headers: authHeader(storage.getStorage('token'))
-            }).then(response => {
-                if (response.data.code === 401)
-                    storage.removeStorage('token');
-                return response.data;
-            }).catch(error => {
-                return error;
-            })
-    }
+    export: () => {
+        return API
+            .get(`/warehouse_operation/export`)
+            .then( res => res.data )
+			.catch( error => error )
+    },
 
-    getListByOption = (sort_option, count, page, search_option) => {
-        return axios
-            .post(`${process.env.REACT_APP_BACKEND_URL}/warehouse_operation/filter_list`, {
+    create: data => {
+        return API
+            .post(`/warehouse_operation`, { data })
+			.then( res => res.data )
+			.catch( error => error )
+    },
+
+    create_list: (data) => {
+        return API
+            .post(`/warehouse_operation/list`, { data })
+			.then( res => res.data )
+			.catch( error => error )
+    },
+
+    getListByOption: (sort_option, count, page, search_option) => {
+        return API
+            .post(`/warehouse_operation/filter_list`, {
                 sort_option: sort_option,
                 count: count,
                 page: page,
                 search_option: search_option
-            }, {
-                headers: authHeader(storage.getStorage('token'))
             })
-            .then(response => {
-                if (response.data.code === 401)
-                    storage.removeStorage('token');
-                return response.data;
-            }).catch(error => {
-                return error;
-            })
-    }
+            .then( res => res.data )
+			.catch( error => error )
+    },
 
-    update = (data, id) => {
-        return axios
-            .put(`${process.env.REACT_APP_BACKEND_URL}/warehouse_operation`, {
-                data: data,
-                id: id
-            }, {
-                headers: authHeader(storage.getStorage('token'))
-            })
-            .then(response => {
-                if (response.data.code === 401)
-                    storage.removeStorage('token');
-                return response.data;
-            }).catch(error => {
-                return error;
-            })
-    }
+    update: ( data, id )  => {
+        return API
+            .put(`/warehouse_operation`, { data, id })
+            .then( res => res.data )
+			.catch( error => error )
+    },
 
-    delete = (id) => {
-        return axios
-            .delete(`${process.env.REACT_APP_BACKEND_URL}/warehouse_operation`, {
-                headers: authHeader(storage.getStorage('token')),
-                params: {
-                    id: id
-                },
-            })
-            .then(response => {
-                if (response.data.code === 401)
-                    storage.removeStorage('token');
-                return response.data;
-            }).catch(error => {
-                return error;
-            })
+    delete: id => {
+        return API
+            .delete(`/warehouse_operation`, { params: { id }})
+            .then( res => res.data )
+			.catch( error => error )
     }
 }
-export default new WarehouseOperation();
+
+export default WarehouseOperation;
