6beeadfbc2bee5aa20dc9adfa03947d948dc8cf5
johnstewart0820 <john.s@outsource.codes>
2021-06-08 12:08:01 +0300

    Breadcrumb component changes

--- src/components/Breadcrumb/Breadcrumb.js
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -1,41 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/styles';
-import { Typography, Link, Breadcrumbs } from '@material-ui/core';
-import clsx from 'clsx';
-
-const useStyles = makeStyles(theme => ({
-	breadcrumb: {
-    color: theme.palette.black,
-		fontFamily: 'roboto',
-		display: 'flex',
-		alignItems: 'center'
-	},
-  link: {
-    color: theme.palette.green,
-    fontSize: '0.8125em',
-  },
-  typo: {
-		color: theme.palette.black,
-    fontSize: '1em',
-  }
-}));
+import "./breadcrumbs.scss";
 
 const Breadcrumb = props => {
-  const { list, parent_class } = props;
 
-  const classes = useStyles();
+	const { list, extra_classes } = props;
 	const title = "Po zmianie opcji wyszukiwania formularz przeładuje się automatycznie";
-  return (
-    <Breadcrumbs aria-label="Ścieżka okruszków" className={clsx(classes.breadcrumb, parent_class)}>
-      {
-        list && list.map((item, index) => (
-          <Typography key={index} color="textPrimary" className={classes.typo} title={title}>{item}</Typography>
-        ))
-      }
-    </Breadcrumbs>
-    
-  );
+  
+	return (
+		<div className={`breadcrumbs ${ extra_classes || ""}`}>
+			{ list && list.map(( item, index ) => (
+				<span key={ index }  title={title}> { item } </span>
+			)) }
+		</div>
+	)
 };
 
 export default Breadcrumb;
